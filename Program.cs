93bb55c4905f00a65a75a1956613d1c9112730d5
RajrupDasid <rajrupofficialid@protonmail.com>
2023-09-13 21:04:52 +0530

    Variables
    How to declrear variables in c#
    
    Signed-off-by: RajrupDasid <rajrupofficialid@protonmail.com>

--- Program.cs
+++ Program.cs
@@ -1,1 +1,18 @@
-﻿
+﻿//first syntax of c#
+// using System;
+// namespace HelloWorld
+// {
+//     class Program
+//     {
+//         static void Main(string[] args)
+//         {
+//             Console.WriteLine(3 + 3);
+//             // Console.Write("Master Blaster");
+//             /*Multi line comment*/
+//         }
+//     }
+
+// }
+
+
+
---
+++ Variables.cs
@@ -0,0 +1,34 @@
+//variables in c#
+
+using System;
+using System.Reflection.PortableExecutable;
+namespace Variables
+{
+    class Variable
+    {
+        static void Main(string[] args)
+        {
+            int myNum;
+            myNum = 45;
+            double dsp = 5.99D;
+            bool mybool = true;
+            char oneletter = 'D';
+            // constants
+            // values cannnot be change in runtime;
+            const int extranumber = 0;
+            /* 
+
+            The const keyword is useful when you want a variable to always store the same value, so that others (or yourself) won't mess up your code. An example that is often referred to as a constant, is PI (3.14159...).
+
+            Note: You cannot declare a constant variable without assigning the value. If you do, an error will occur: A const field requires a value to be provided.
+            */
+
+            Console.WriteLine(myNum);
+            Console.WriteLine("double" + ' ' + dsp); // string conncatination
+            Console.WriteLine(mybool);
+            Console.WriteLine(oneletter);
+            Console.WriteLine(extranumber);
+
+        }
+    }
+}
