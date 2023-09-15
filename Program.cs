ac0d63be636ec88dd4128cdabd7a08b873acc9c1
RajrupDasid <rajrupofficialid@protonmail.com>
2023-09-15 20:13:45 +0530

    User input
    How to take user input in console apps
    
    Signed-off-by: RajrupDasid <rajrupofficialid@protonmail.com>

--- Program.cs
+++ Program.cs
@@ -1,1 +1,18 @@
-﻿
+﻿//Get user Input
+using System;
+namespace UserInput
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your username");
+            string userName = Console.ReadLine();
+            Console.WriteLine("Your Username is :: " + ' ' + userName);
+            Console.WriteLine("Enter your Age");
+            // Console.readline always inputs string data so we have to externally convert it to the desired user type
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Your age is : " + ' ' + age);
+        }
+    }
+}
