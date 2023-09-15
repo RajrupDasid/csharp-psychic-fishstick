21ee40fc0bf484159b9ac8bfced2a8944cec2333
RajrupDasid <rajrupofficialid@protonmail.com>
2023-09-15 19:20:47 +0530

    Typecasting
    How typecasting occurs in C#
    
    Signed-off-by: RajrupDasid <rajrupofficialid@protonmail.com>

--- Program.cs
+++ Program.cs
@@ -1,1 +1,33 @@
-﻿
+﻿//c# typecasting 
+//typecasting is change the type of the incoming data as required by code logics
+/*
+C# Type Casting
+Type casting is when you assign a value of one data type to another type.
+
+In C#, there are two types of casting:
+
+Implicit Casting (automatically) - converting a smaller type to a larger type size
+char -> int -> long -> float -> double
+
+Explicit Casting (manually) - converting a larger type to a smaller size type
+double -> float -> long -> int -> char
+*/
+using System;
+namespace TypeCasting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int myInt = 10;
+            double myDouble = 5.25;
+            bool myBool = true;
+
+            Console.WriteLine(Convert.ToString(myInt));    // convert int to string
+            Console.WriteLine(Convert.ToDouble(myInt));    // convert int to double
+            Console.WriteLine(Convert.ToInt32(myDouble));  // convert double to int
+            Console.WriteLine(Convert.ToString(myBool));   // convert bool to string
+
+        }
+    }
+}
